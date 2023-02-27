ff35d881537454908e478ad8cf88834885cee6f8
Tessa <118585625+tessarakkt@users.noreply.github.com>
2023-02-27 04:15:25 -0400

    correct LOD Cull Enabled button not referencing quadtree properly

--- example/OceanUI.gd
+++ example/OceanUI.gd
@@ -77,7 +77,7 @@ func _on_wave_speed_value_changed(value:float) -> void:
 
 
 func _on_cull_enabled_toggled(button_pressed:bool) -> void:
-	$QuadTree3D.pause_cull = not button_pressed
+	$"../QuadTree3D".pause_cull = not button_pressed
 
 
 func _on_planetary_curve_value_changed(value:float) -> void:
