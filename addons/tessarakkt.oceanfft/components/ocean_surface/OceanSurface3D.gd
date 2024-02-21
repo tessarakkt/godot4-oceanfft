14fc65bf788d323a36e6f070cf04b434dd834030
Matt O'Tousa <mlotousa@gmail.com>
2024-02-21 12:23:28 -0800

    OceanSurface3D infinite rendering options

--- addons/tessarakkt.oceanfft/components/ocean_surface/OceanSurface3D.gd
+++ addons/tessarakkt.oceanfft/components/ocean_surface/OceanSurface3D.gd
@@ -3,6 +3,14 @@ extends Node3D
 class_name OceanSurface3D
 ## Base OceanSurface3D class. Has no rendering.
 
+enum InfiniteMode {
+	OFF,
+	PROCESS,
+	PHYSICS_PROCESS,
+}
+
+@export var infinite_mode: InfiniteMode
+
 @export var ocean: Ocean3D:
 	set(value):
 		ocean = value
@@ -15,5 +23,18 @@ func _enter_tree():
 	if get_parent() is Ocean3D:
 		ocean = get_parent()
 
+## Override this to implement rendering in subclasses
 func _on_set_ocean() -> void:
 	pass
+
+func _process(delta: float):
+	if infinite_mode == InfiniteMode.PROCESS:
+		_position_at_camera_xz()
+
+func _physics_process(delta: float):
+	if infinite_mode == InfiniteMode.PHYSICS_PROCESS:
+		_position_at_camera_xz()
+
+func _position_at_camera_xz():
+	position.x = ocean.camera.position.x
+	position.z = ocean.camera.position.z
